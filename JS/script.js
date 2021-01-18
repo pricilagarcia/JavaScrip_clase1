d910da751409e12132af80b1193888e299bfabdc
pricilagarcia <68708787+pricilagarcia@users.noreply.github.com>
2021-01-18 10:38:33 -0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+//VARIABLES Y TIPOS DE DATOS EN JAVASCRI
+var saludo = "Hola JavaScrip"
+var edad = 26
+var siNo = true
+var seccion = 'a'
+
+
+//MOSTRAR UN DATO EN CONSOLA
+console.log (saludo)
+
+
+var datoApellido = document.getElementById('favorito')
+
+
+
